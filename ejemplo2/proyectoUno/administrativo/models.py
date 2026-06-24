a56358685013a4531edf2eeecb97e1e44eef3413
iSkrAaleh <alexsantiagoserranosaca@gmail.com>
2026-06-24 13:49:11 -0500

    changes

--- ejemplo2/proyectoUno/administrativo/models.py
+++ ejemplo2/proyectoUno/administrativo/models.py
@@ -12,6 +12,9 @@ class Estudiante(models.Model):
                 self.apellido,
                 self.cedula)
 
+    def obtener_numero_telefonicos(self):
+        return self.numeros_telefonicos.count()
+
 class NumeroTelefonico(models.Model):
     telefono = models.CharField(max_length=100)
     tipo = models.CharField(max_length=100)
@@ -21,3 +24,16 @@ class NumeroTelefonico(models.Model):
     def __str__(self):
         return "%s %s" % (self.telefono, self.tipo)
 
+    def obtener_operadora(self):
+        # si el teléfono empieza con 099
+        if self.telefono.startswith('099'):
+            return "Movistar"
+        # si el teléfono empieza con 098
+        elif self.telefono.startswith('098'):
+            return "Claro"
+        # si empieza con 09 pero no fue ni 099 ni 098
+        elif self.telefono.startswith('09'):
+            return "Otra operadora"
+        # en caso de que sea un teléfono fijo u otro formato
+        else:
+            return "Desconocida"
